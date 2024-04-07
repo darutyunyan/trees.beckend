7366791994c3f3de8f9f88d1b2b426e8b9262809
Daniel <darutyunyan14@yandex.com>
2024-04-07 11:10:13 +0300

    Add swagger UI

--- Trees.Api/Program.cs
+++ Trees.Api/Program.cs
@@ -30,7 +30,16 @@ builder.Services.AddAutoMapper(typeof(ApiMappingProfile), typeof(DbMappingProfil
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
